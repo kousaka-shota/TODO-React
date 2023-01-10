e007b86aed22961187b04ee9be723ea375717ce8
Shota Kousaka <114033759+kousaka-shota@users.noreply.github.com>
2023-01-10 16:18:35 +0900

    kkk

--- src/Apps.jsx
+++ src/Apps.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
+  const onClickButton = () => alert();
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfullMessage color="blue" message="お元気ですか" />
+      <ColorfullMessage color="pink" message="元気です" />
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
